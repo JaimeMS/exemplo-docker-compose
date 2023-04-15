6beb642f23457f9380539d2a3ad473e77569f9c3
Jaime <santos.jaimemendes91@gmail.com>
2023-04-15 10:35:18 -0300

    primeiro commit

---
+++ backend/app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const restful = require('node-restful')
+const server = express()
+const mongoose = restful.mongoose
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+//Database
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://db/mydb')
+
+//Test
+//server.get('/', (req, res, next) => res.send('Backend'))
+
+//Middlewares
+server.use(bodyParser.urlencoded({extended:true}))
+server.use(bodyParser.json())
+server.use(cors())
+
+// ODM
+const Client = restful.model('Client', {
+    name: { type: String, require: true }
+})
+
+// Rest API
+Client.methods(['get', 'post', 'put', 'delete'])
+Client.updateOptions({new: true, reunValidators: true})
+
+// Routes
+Client.register(server, '/clients')
+
+//Start Server
+server.listen(3000)
